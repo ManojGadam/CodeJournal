ab5d7ab8c8125b9297b19390941a920b7f43f3e5
Manoj Gadamchetty <64482839+ManojGadam@users.noreply.github.com>
2025-02-03 01:24:33 -0500

    s

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddScoped<IProblemRepository, ProblemRepository>();
 builder.Services.AddDbContext<ProblemContext>(options =>
     {
         var connectionString = builder.Configuration.GetConnectionString("Deployment");
-         //if (!builder.Environment.IsDevelopment()) 
+         //if (!builder.Environment.IsDevelopment())
          //   {
          //       var password = Environment.GetEnvironmentVariable("MSSQL_SA_PASSWORD");
          //       Console.WriteLine($"Password from env: {connectionString} , {password}");
